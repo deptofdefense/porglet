5dfc39429464fde1a2dca659603feaf70908f8f7
Nick Ashworth <jna2188@utulsa.edu>
2021-01-25 23:52:37 -0600

    added flowgraph

--- support/breakout.py
+++ support/breakout.py
@@ -1,12 +1,23 @@
 import numpy # needed for IQ
 
-myfile = open("Parrot_DISCO_2440_0300.16sc", "r")
-#testfile = open("test.cfile", "wb")
+myfile = open("Parrot_DISCO_2440_0300.cs16", "rb")
+testfile = open("test.cfile", "wb")
+testfile2 = open("test2.cfile", "wb")
+
 
 dat = numpy.fromfile(myfile, dtype=numpy.int16)
 
 datFloat = dat.astype(numpy.float32)
+datS = datFloat.newbyteorder('S')
 
-datFloat.tofile("test.cfile")
+#datFloat.tofile(testfile)
 
+
+data = datFloat.tobytes()
+data2 = datS.tobytes()
+
+testfile.write(data)
+testfile2.write(data2)
+
+testfile.close()
 myfile.close()
